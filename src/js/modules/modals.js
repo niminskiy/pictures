c15d9b1c802317f37b87680f06771888d638782c
niminskiy <s.niminskiy@gmail.com>
2020-12-01 15:57:20 +0300

    modal

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -19,7 +19,6 @@ const modals = () => {
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
         document.body.style.marginRight = `${scroll}px`;
-        // document.body.classList.add('modal-open');
       });
     });
 
@@ -31,7 +30,6 @@ const modals = () => {
       modal.style.display = "none";
       document.body.style.overflow = "";
       document.body.style.marginRight = `0px`;
-      // document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
@@ -43,15 +41,24 @@ const modals = () => {
         modal.style.display = "none";
         document.body.style.overflow = "";
         document.body.style.marginRight = `0px`;
-        // document.body.classList.remove('modal-open');
       }
     });
   }
 
   function showModalByTime(selector, time) {
     setTimeout(function() {
-      document.querySelector(selector).style.display = "block";
-      document.body.style.overflow = "hidden";
+      let display;
+
+      document.querySelectorAll('[data-modal]').forEach(item => {
+        if (getComputedStyle(item).display !== 'none') {
+          display = 'block';
+        }
+      });
+
+      if (!display) {
+        document.querySelector(selector).style.display = 'block';
+        document.body.style.overflow = 'hidden';
+      }
     }, time);
   }
 
@@ -70,11 +77,9 @@ const modals = () => {
     return scrollWidth;
   }
 
-  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-  bindModal('.phone_link', '.popup', '.popup .popup_close');
-  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
-  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
-  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+  bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+  bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+  
   // showModalByTime('.popup', 60000);
 };
 
